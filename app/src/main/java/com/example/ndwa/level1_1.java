48958a85be383c19f16827c5ffd0e5d667917c87
DinaAljutayli <dinaljutayli@gmail.com>
2020-11-20 20:53:30 +0300

    add if else in leve1.1

--- app/src/main/java/com/example/ndwa/level1_1.java
+++ app/src/main/java/com/example/ndwa/level1_1.java
@@ -1,25 +1,81 @@
 package com.example.ndwa;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.content.Context;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 public class level1_1 extends AppCompatActivity {
-    Button help;
-    Context context;
+   // Button help;
+    //Context context;
+    EditText ruselt;
+    Button Save2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_1);
-        help=(Button)findViewById(R.id.wanthelp1_1);
-        if(help.callOnClick()){
-            helper(context);
+     /*   help=(Button)findViewById(R.id.wanthelp1_1);
+           if(help.callOnClick()){
+               helper(context);*/
+            ruselt = findViewById(R.id.ruselt1);
+            Save2 = findViewById(R.id.Save2);
+
         }
+        public void wanthelp1_1(View view){
+
+            AlertDialog dialog = new AlertDialog.Builder(level1_1.this)
+                    .setTitle("Help")
+                    .setMessage("Hint: One of computers filed")
+                    .setNegativeButton("ok",null)
+                    .show();
+        }
+
+
+        public void Save2(View view){
+           String result = ruselt.getText().toString();
+            if(result == "info security"){
+
+                AlertDialog dialog = new AlertDialog.Builder(level1_1.this)
+                        .setTitle("You Are Winner Go To Next Level")
+                        .setMessage("Result:073078070079032083069067085082073084089")
+                        .setNegativeButton("ok",null)
+                        .show();
+                Intent levelIntent = new Intent(level1_1.this, level1_2.class);
+                startActivity(levelIntent);
+
+            }
+            else{
+                AlertDialog dialogl = new AlertDialog.Builder(level1_1.this)
+                        .setTitle("Loser!!!")
+                        .setMessage("Try Again...")
+                        .setNegativeButton("ok",null)
+                        .show();
+            }
+
+
     }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     //نحتاج ميثود قبلها تستدعي الميثود هذه اذا كانت الاجابة صحيحة
 
-        public static void show(Context context) {
+      /*  public static void show(Context context) {
             new AlertDialog.Builder(context)
                     .setTitle("You Are Winner Go To Next Level")
                     .setMessage("Result:073078070079032083069067085082073084089")
@@ -31,7 +87,9 @@ public class level1_1 extends AppCompatActivity {
                     // A null listener allows the button to dismiss the dialog and take no further action.
                     .setIcon(android.R.drawable.btn_star_big_on)
                     .show();
-        }
+        } */
+
+      /*
     public static void helper(Context context) {
         new AlertDialog.Builder(context)
                 .setTitle("HELP")
@@ -45,4 +103,6 @@ public class level1_1 extends AppCompatActivity {
                 .setIcon(android.R.drawable.ic_lock_idle_lock)
                 .show();
     }
-    }
+    }*/
+
+
--- app/src/main/java/com/example/ndwa/level1_2.java
+++ app/src/main/java/com/example/ndwa/level1_2.java
@@ -4,24 +4,92 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 public class level1_2 extends AppCompatActivity {
-    Button help;
-    Context context;
+    /*Button help;
+    Context context;*/
+    EditText ruselt2;
+    Button Save;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_2);
-        help=(Button)findViewById(R.id.wanthelp1_2);
+      /*  help=(Button)findViewById(R.id.wanthelp1_2);
         if(help.callOnClick()){
-            helper1_2(context);
-    }}
+            helper1_2(context);*/
+
+      ruselt2 = findViewById(R.id.ruselt2);
+      Save= findViewById(R.id.Save2);
+      Save.setOnClickListener(view -> {
+
+          String result2 = ruselt2.getText().toString();
+          if(result2 == "DINA"){
+              AlertDialog dialog = new AlertDialog.Builder(level1_2.this)
+                      .setTitle("You Are Winner Go To Next Level")
+                      .setMessage("ASCII Code TO TEXT Result is :DINA")
+                      .setNegativeButton("ok",null)
+                      .show();
+
+              Intent level2Intent = new Intent(level1_2.this, level2_2.class);
+              startActivity(level2Intent);
+          }
+          else{
+              AlertDialog dialogl = new AlertDialog.Builder(level1_2.this)
+                      .setTitle("Loser!!!")
+                      .setMessage("Try Again...")
+                      .setNegativeButton("ok",null)
+                      .show();
+          }
+
+
+      });
+
+    }
+
+    public void wanthelp1_2(View view) {
+
+        AlertDialog dialog = new AlertDialog.Builder(level1_2.this)
+                .setTitle("Help")
+                .setMessage("ASCII")
+                .setNegativeButton("ok", null)
+                .show();
+    }
+
+
+  /*  public void Save(View view) {
+
+        String result2 = ruselt2.getText().toString();
+        if(result2 == "DINA"){
+
+            AlertDialog dialog = new AlertDialog.Builder(level1_2.this)
+                    .setTitle("You Are Winner Go To Next Level")
+                    .setMessage("ASCII Code TO TEXT Result is :DINA")
+                    .setNegativeButton("ok",null)
+                    .show();
+
+            Intent level2Intent = new Intent(level1_2.this, level2_2.class);
+            startActivity(level2Intent);
+        }
+
+
+    }*/
+}
+
+
+
+    /*
     //نحتاج ميثود قبلها تستدعي الميثود هذه اذا كانت الاجابة صحيحة
     public static void show(Context context) {
         new AlertDialog.Builder(context)
                 .setTitle("You Are Winer Go To Next Level")
-                .setMessage("Hint :ASCII Code TO TEXT Result is :DANA")
+                .setMessage("Hint :ASCII Code TO TEXT Result is :DINA")
                 // Specifying a listener allows you to take an action before dismissing the dialog.
                 // The dialog is automatically dismissed when a dialog button is clicked.
                 .setPositiveButton("Ok", (dialog, which) -> {
@@ -44,4 +112,4 @@ public class level1_2 extends AppCompatActivity {
                     .setIcon(android.R.drawable.ic_lock_idle_lock)
                     .show();
         }
-}
+}*/
